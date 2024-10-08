4f3781e7555340218f56b077a81e99f5e3cbb36a
Just253 <70507145+Just253@users.noreply.github.com>
2024-10-08 00:59:26 -0500

    Actualizar vista de lista de lugares y mapa
    - Actualizar la vista de lista de lugares para mostrar una imagen del lugar junto a su nombre.
    - Agregar soporte para desplazamiento en la vista de lista de lugares.
    - Agregar soporte para mostrar imágenes de los lugares en la vista de mapa.
    - Agregar soporte para typeado de Google Maps en el proyecto.
    - Actualizar script de desarrollo para incluir opción de host en Vite.
    - Agregar soporte para Google Maps en la vista de Mapa y Lugares.

--- frontend/src/components/ListPlaces.vue
+++ frontend/src/components/ListPlaces.vue
@@ -2,9 +2,9 @@
   <div id="listPlaces">
     <h2 class="text-2xl font-bold mb-4 text-gray-600">Lugares cercanos</h2>
     <ul id="ul-places" class="space-y-4 overflow-auto">
-      <li v-for="place in places" :key="place.id" class="flex items-start" @click="selectPlace(place)">
-        <div class="bg-gray-200 rounded-lg p-2 mr-4">
-          <MapPinIcon class="w-6 h-6 text-gray-600" />
+      <li v-for="place in placesStore.places" :key="place.id" class="flex items-start" @click="selectPlace(place)">
+        <div class="bg-gray-200 rounded-lg p-2 mr-2">
+          <img :src="place.image" alt="Imagen del lugar" class="place-image" />
         </div>
         <div>
           <h3 class="font-semibold text-gray-600">{{ place.id + " | " + place.name }}</h3>
@@ -16,38 +16,11 @@
 </template>
 
 <script setup lang="ts">
-import { MapPinIcon } from 'lucide-vue-next'
 import { ref, onMounted, onBeforeUnmount } from 'vue'
 import { useRouter } from 'vue-router'
+import { usePlacesStore } from '@/stores/places'
 
-const base_cords_TEST = {
-  lat: -12.1955305,
-  lon: -76.9713426
-}
-
-const diff = 0.001
-
-const places = ref([
-  { id: 1, name: 'Universidad Autonoma del Peru [UA]', address: 'Panamericana Sur Km. 16.3, Villa EL Salvador, Perú', lat: base_cords_TEST.lat - diff * 1, lon: base_cords_TEST.lon - diff * 1 },
-  { id: 2, name: 'Universidad Tecnologica del Peru [UTP]', address: 'Ctra. Panamericana Sur km 16, Villa EL Salvador 15842, Perú', lat: -12.193359, lon:  -76.972071 },
-  { id: 3, name: 'Makro Villa El Salvador', address: 'Cruce Panamericana Sur con Av. Mateo Pumacahua, (Frente al Parque Zonal Huayna Cápac), Lima 15842, Perú', lat: -12.1930825, lon: -76.9712553 },
-  { id: 4, name: 'SISOL Salud Villa el Salvador', address: 'Av. Mariano Pastor Sevilla S/N, Villa EL Salvador 15829, Perú', lat: -12.1964846, lon: -76.9590056 },
-  { id: 5, name: 'Mercado Unicachi', address: 'Avenida los algarrobos', lat:-12.2033776 , lon:-76.9652853},
-  { id: 6, name: 'Universidad Tecnologica del Peru [UTP]', address: 'Ctra. Panamericana Sur km 16, Villa EL Salvador 15842, Perú', lat: -12.193359, lon:  -76.972071 },
-  { id: 7, name: 'Makro Villa El Salvador', address: 'Cruce Panamericana Sur con Av. Mateo Pumacahua, (Frente al Parque Zonal Huayna Cápac), Lima 15842, Perú', lat: -12.1930825, lon: -76.9712553 },
-  { id: 8, name: 'SISOL Salud Villa el Salvador', address: 'Av. Mariano Pastor Sevilla S/N, Villa EL Salvador 15829, Perú', lat: -12.1964846, lon: -76.9590056 },
-  { id: 9, name: 'Mercado Unicachi', address: 'Avenida los algarrobos', lat:-12.2033776 , lon:-76.9652853},
-  { id: 10, name: 'Universidad Tecnologica del Peru [UTP]', address: 'Ctra. Panamericana Sur km 16, Villa EL Salvador 15842, Perú', lat: -12.193359, lon:  -76.972071 },
-  { id: 11, name: 'Makro Villa El Salvador', address: 'Cruce Panamericana Sur con Av. Mateo Pumacahua, (Frente al Parque Zonal Huayna Cápac), Lima 15842, Perú', lat: -12.1930825, lon: -76.9712553 },
-  { id: 12, name: 'SISOL Salud Villa el Salvador', address: 'Av. Mariano Pastor Sevilla S/N, Villa EL Salvador 15829, Perú', lat: -12.1964846, lon: -76.9590056 },
-  { id: 13, name: 'Mercado Unicachi', address: 'Avenida los algarrobos', lat:-12.2033776 , lon:-76.9652853},
-  { id: 14, name: 'Universidad Tecnologica del Peru [UTP]', address: 'Ctra. Panamericana Sur km 16, Villa EL Salvador 15842, Perú', lat: -12.193359, lon:  -76.972071 },
-  { id: 15, name: 'Makro Villa El Salvador', address: 'Cruce Panamericana Sur con Av. Mateo Pumacahua, (Frente al Parque Zonal Huayna Cápac), Lima 15842, Perú', lat: -12.1930825, lon: -76.9712553 },
-  { id: 16, name: 'SISOL Salud Villa el Salvador', address: 'Av. Mariano Pastor Sevilla S/N, Villa EL Salvador 15829, Perú', lat: -12.1964846, lon: -76.9590056 },
-  { id: 17, name: 'Mercado Unicachi', address: 'Avenida los algarrobos', lat:-12.2033776 , lon:-76.9652853},
-  
-])
-
+const placesStore = usePlacesStore()
 const router = useRouter()
 
 const selectPlace = (place) => {
@@ -62,6 +35,7 @@ const adjustUlPlacesHeight = () => {
     ulPlaces.style.height = `${listPlaces.style.height }px`
   }
 }
+
 
 onMounted(() => {
   adjustUlPlacesHeight()
@@ -88,4 +62,12 @@ onBeforeUnmount(() => {
   flex-grow: 1;
   overflow-y: auto;
 }
+
+.place-image {
+  width: 50px;
+  height: 40px;
+  object-fit: cover;
+  margin-right: 0.5rem;
+}
+
 </style>
--- frontend/src/components/MapAndPlaces.vue
+++ frontend/src/components/MapAndPlaces.vue
@@ -1,31 +1,32 @@
 <template>
   <div class="flex flex-col relative">
-    <!-- Mapa -->
-    <GoogleMap
-      ref="mapRef" 
-      class="relative flex-grow transition-all duration-300 ease-in-out"
-      mapId="DEMO_MAP_ID"
-      style="height: 100vh;" 
-      :api-key="apiKey"
-      :center="center"
-      :zoom="15"
-    >
-      <AdvancedMarker :options="markerOptions" :pin-options="pinOptions"/>
+    <!-- TODO: FIX CUSTOM MARKER  -->
+    <GoogleMap ref="mapRef" class="relative flex-grow transition-all duration-300 ease-in-out" mapId="DEMO_MAP_ID"
+      style="height: 100vh;" :api-key="apiKey" :center="center" :zoom="15" @zoom_changed="">
+      <CustomMarker v-for="place in placesStore.places" :key="place.id" :options="{ position: { lat: place.lat, lng: place.lon }, anchorPoint: 'BOTTOM_CENTER' }">
+        <div style="text-align: center">
+          <div style="font-size: 1.125rem">{{ place.name }}</div>
+          <img :src="place.image" 
+            width="50" height="50" style="margin-top: 8px" />
+        </div>
+      </CustomMarker>
     </GoogleMap>
-    
+
   </div>
 </template>
 
 <script setup lang="ts">
-import { GoogleMap, AdvancedMarker } from 'vue3-google-map'
+import { GoogleMap, CustomMarker } from 'vue3-google-map'
 import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
+import { usePlacesStore } from '@/stores/places'
 
 const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
 const center = { lat: -12.1955305, lng: -76.9713426 }
-const markerOptions = { position: center, title: 'LADY LIBERTY' }
-const pinOptions = { background: '#FBBC04' }
+const markerOptions = { position: center}
 const mapRef =  ref<InstanceType<typeof GoogleMap> | null>(null)
+const placesStore = usePlacesStore()
+
 
 const route = useRoute()
 watch(() => route.query, (newQuery) => {
