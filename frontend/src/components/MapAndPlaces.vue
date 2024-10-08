f2db0b83f0b651a8003bf825db615903b04e516f
Just253 <70507145+Just253@users.noreply.github.com>
2024-10-07 21:48:36 -0500

    Agregar soporte para Google Maps en la vista de Mapa y Lugares

--- frontend/src/components/MapAndPlaces.vue
+++ frontend/src/components/MapAndPlaces.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="flex flex-col relative">
     <!-- Mapa -->
-    <div 
+    <GoogleMap 
       class="relative flex-grow transition-all duration-300 ease-in-out"
+      mapId="DEMO_MAP_ID"
       style="height: 100vh;" 
-      >
-      <!-- Ajusta el valor de vh según sea necesario -->
-      <img 
-        src="https://i.supa.codes/2_xIX" 
-        alt="Mapa" 
-        class="w-full h-full object-cover"
-      />
-    </div>
-
-    <!-- Lista de lugares -->
+      :api-key="apiKey"
+      :center="center"
+      :zoom="15"
+    >
+      <AdvancedMarker :options="markerOptions" :pin-options="pinOptions"/>
+    </GoogleMap>
     
   </div>
 </template>
+
+<script setup>
+import { GoogleMap, AdvancedMarker } from 'vue3-google-map'
+
+const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
+const center = { lat: 40.689247, lng: -74.044502 }
+const markerOptions = { position: center, title: 'LADY LIBERTY' }
+const pinOptions = { background: '#FBBC04' }
+</script>
 
 <style scoped>
 /* Estilos adicionales si son necesarios */
