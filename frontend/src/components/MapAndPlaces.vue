59681e7f82293d396b73a982bfdb26387bc09893
Just253 <70507145+Just253@users.noreply.github.com>
2024-10-07 23:26:34 -0500

    Agregar soporte para typeado de Google Maps en el proyecto

--- frontend/src/components/MapAndPlaces.vue
+++ frontend/src/components/MapAndPlaces.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="flex flex-col relative">
     <!-- Mapa -->
-    <GoogleMap 
+    <GoogleMap
+      ref="mapRef" 
       class="relative flex-grow transition-all duration-300 ease-in-out"
       mapId="DEMO_MAP_ID"
       style="height: 100vh;" 
@@ -15,13 +16,27 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { GoogleMap, AdvancedMarker } from 'vue3-google-map'
+import { ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
 
 const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
-const center = { lat: 40.689247, lng: -74.044502 }
+const center = { lat: -12.1955305, lng: -76.9713426 }
 const markerOptions = { position: center, title: 'LADY LIBERTY' }
 const pinOptions = { background: '#FBBC04' }
+const mapRef =  ref<InstanceType<typeof GoogleMap> | null>(null)
+
+const route = useRoute()
+watch(() => route.query, (newQuery) => {
+  const lat = newQuery.lat
+  const lon = newQuery.lon
+  if (typeof lat === 'string' && typeof lon === 'string' && mapRef.value) {
+    mapRef?.value?.map?.panTo({ lat: parseFloat(lat), lng: parseFloat(lon) })
+    mapRef?.value?.map?.setZoom(18)
+  }
+}, { immediate: true })
+
 </script>
 
 <style scoped>
