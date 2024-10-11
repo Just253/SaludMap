06ba75db18b3c799e0faf6a80fb1228d47fb357b
Just253 <70507145+Just253@users.noreply.github.com>
2024-10-10 21:14:08 -0500

    Actualizar tipo de lugares a "animal" y "humano"

--- frontend/src/stores/places.ts
+++ frontend/src/stores/places.ts
@@ -21,14 +21,16 @@ export const usePlacesStore = defineStore('places', () => {
         address: "Ctra. Panamericana Sur km 16, Villa EL Salvador 15842, Perú",
         lat: -12.193359,
         lng: -76.972071,
+        type: "animal"
       },
       {
         id: 3,
         name: "Makro Villa El Salvador",
         address:
           "Cruce Panamericana Sur con Av. Mateo Pumacahua, (Frente al Parque Zonal Huayna Cápac), Lima 15842, Perú",
-        lat: -12.1930825,
+        lat: -12.1950825,
         lng: -76.9712553,
+        type: "human"
       },
       {
         id: 4,
@@ -37,6 +39,7 @@ export const usePlacesStore = defineStore('places', () => {
           "Av. Mariano Pastor Sevilla S/N, Villa EL Salvador 15829, Perú",
         lat: -12.1964846,
         lng: -76.9590056,
+        type: "animal"
       },
       {
         id: 5,
@@ -44,97 +47,7 @@ export const usePlacesStore = defineStore('places', () => {
         address: "Avenida los algarrobos",
         lat: -12.2033776,
         lng: -76.9652853,
-      },
-      {
-        id: 6,
-        name: "Universidad Tecnologica del Peru [UTP]",
-        address: "Ctra. Panamericana Sur km 16, Villa EL Salvador 15842, Perú",
-        lat: -12.193359,
-        lng: -76.972071,
-      },
-      {
-        id: 7,
-        name: "Makro Villa El Salvador",
-        address:
-          "Cruce Panamericana Sur con Av. Mateo Pumacahua, (Frente al Parque Zonal Huayna Cápac), Lima 15842, Perú",
-        lat: -12.1930825,
-        lng: -76.9712553,
-      },
-      {
-        id: 8,
-        name: "SISOL Salud Villa el Salvador",
-        address:
-          "Av. Mariano Pastor Sevilla S/N, Villa EL Salvador 15829, Perú",
-        lat: -12.1964846,
-        lng: -76.9590056,
-      },
-      {
-        id: 9,
-        name: "Mercado Unicachi",
-        address: "Avenida los algarrobos",
-        lat: -12.2033776,
-        lng: -76.9652853,
-      },
-      {
-        id: 10,
-        name: "Universidad Tecnologica del Peru [UTP]",
-        address: "Ctra. Panamericana Sur km 16, Villa EL Salvador 15842, Perú",
-        lat: -12.193359,
-        lng: -76.972071,
-      },
-      {
-        id: 11,
-        name: "Makro Villa El Salvador",
-        address:
-          "Cruce Panamericana Sur con Av. Mateo Pumacahua, (Frente al Parque Zonal Huayna Cápac), Lima 15842, Perú",
-        lat: -12.1930825,
-        lng: -76.9712553,
-      },
-      {
-        id: 12,
-        name: "SISOL Salud Villa el Salvador",
-        address:
-          "Av. Mariano Pastor Sevilla S/N, Villa EL Salvador 15829, Perú",
-        lat: -12.1964846,
-        lng: -76.9590056,
-      },
-      {
-        id: 13,
-        name: "Mercado Unicachi",
-        address: "Avenida los algarrobos",
-        lat: -12.2033776,
-        lng: -76.9652853,
-      },
-      {
-        id: 14,
-        name: "Universidad Tecnologica del Peru [UTP]",
-        address: "Ctra. Panamericana Sur km 16, Villa EL Salvador 15842, Perú",
-        lat: -12.193359,
-        lng: -76.972071,
-      },
-      {
-        id: 15,
-        name: "Makro Villa El Salvador",
-        address:
-          "Cruce Panamericana Sur con Av. Mateo Pumacahua, (Frente al Parque Zonal Huayna Cápac), Lima 15842, Perú",
-        lat: -12.1930825,
-        lng: -76.9712553,
-      },
-      {
-        id: 16,
-        name: "SISOL Salud Villa el Salvador",
-        address:
-          "Av. Mariano Pastor Sevilla S/N, Villa EL Salvador 15829, Perú",
-        lat: -12.1964846,
-        lng: -76.9590056,
-      },
-      {
-        id: 17,
-        name: "Mercado Unicachi",
-        address: "Avenida los algarrobos",
-        lat: -12.2033776,
-        lng: -76.9652853,
-      },
+      }
     ]);
 
     function addPlace(place: Place) {
