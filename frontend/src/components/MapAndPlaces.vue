26e4a8a934b1a53db1b8a9f76df20d1839838243
Just253 <70507145+Just253@users.noreply.github.com>
2024-10-10 21:13:42 -0500

    Actualizar vista de lista de lugares y mapa

--- frontend/src/components/MapAndPlaces.vue
+++ frontend/src/components/MapAndPlaces.vue
@@ -1,39 +1,56 @@
 <template>
   <div class="flex flex-col relative">
     <!-- TODO: FIX CUSTOM MARKER  -->
-    <GoogleMap ref="mapRef" class="relative flex-grow transition-all duration-300 ease-in-out" mapId="DEMO_MAP_ID"
-      style="height: 100vh;" :api-key="apiKey" :center="center" :zoom="15" @zoom_changed="">
-      <CustomMarker v-for="place in placesStore.places" :key="place.id" :options="{ position: { lat: place.lat, lng: place.lon }, anchorPoint: 'BOTTOM_CENTER' }">
-        <div style="text-align: center">
-          <div style="font-size: 1.125rem">{{ place.name }}</div>
-          <img :src="place.image" 
-            width="50" height="50" style="margin-top: 8px" />
-        </div>
-      </CustomMarker>
+    <GoogleMap 
+      ref="mapRef" 
+      class="relative flex-grow transition-all duration-300 ease-in-out" 
+      mapId="bc66a77ddf9efcd5"
+      style="height: 90vh;" 
+      :api-key="apiKey" 
+      :center="center" 
+      :zoom="15" 
+    >
+      <AdvancedMarker 
+        v-for="place in placesStore.places" 
+        :key="place.id" 
+        :options="{ position: { lat: place.lat, lng: place.lng }}" 
+        :pin-options="{ background: getColor(place) }"
+      >
+        <p>{{ place.type }}</p>
+      </AdvancedMarker>
     </GoogleMap>
 
   </div>
 </template>
 
 <script setup lang="ts">
-import { GoogleMap, CustomMarker } from 'vue3-google-map'
+import { GoogleMap, AdvancedMarker } from 'vue3-google-map'
 import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { usePlacesStore } from '@/stores/places'
 
 const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
 const center = { lat: -12.1955305, lng: -76.9713426 }
-const markerOptions = { position: center}
 const mapRef =  ref<InstanceType<typeof GoogleMap> | null>(null)
 const placesStore = usePlacesStore()
 
+const colors = {
+  animal: '#FFA500',
+  human: '#00BFFF',
+  default: '#ff0000'
+}
+
+const getColor = (place: Place) => {
+  console.log(place.type)
+  return place.type && colors[place.type] ? colors[place.type] : colors.default
+}
 
 const route = useRoute()
 watch(() => route.query, (newQuery) => {
   const lat = newQuery.lat
-  const lon = newQuery.lon
-  if (typeof lat === 'string' && typeof lon === 'string' && mapRef.value) {
-    mapRef?.value?.map?.panTo({ lat: parseFloat(lat), lng: parseFloat(lon) })
+  const lng = newQuery.lng
+  if (typeof lat === 'string' && typeof lng === 'string' && mapRef.value) {
+    mapRef?.value?.map?.panTo({ lat: parseFloat(lat), lng: parseFloat(lng) })
     mapRef?.value?.map?.setZoom(18)
   }
 }, { immediate: true })
@@ -41,5 +58,4 @@ watch(() => route.query, (newQuery) => {
 </script>
 
 <style scoped>
-/* Estilos adicionales si son necesarios */
 </style>
